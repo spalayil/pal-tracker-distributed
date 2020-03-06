0197f5157fb43a5da982bd13a464053d080da01c
Sreejith <sreejith.p5@cognizant.com>
2020-03-06 14:23:29 +0530

    added import for EnableEurekaClient

--- applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/App.java
+++ applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/App.java
@@ -3,6 +3,7 @@ package io.pivotal.pal.tracker.registration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import java.util.TimeZone;
 
